b6849413a9b4fc6ad39742ff1e6e5d1e622c225b
sedo2000 <sydwbydw@gmail.com>
2025-09-10 23:57:41 +0300

    Create send.php

---
+++ send.php
@@ -0,0 +1,10 @@
+<?php
+$token = "7482818508:AAE0HJwhkXcyGRGnmsE8Ahgnrf5N9n1mL1g";
+$chat_id = "@IPADDRESSBSG";
+
+$message = isset($_POST['message']) ? $_POST['message'] : "No data";
+
+file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=".urlencode($message));
+
+echo json_encode(["status"=>"success"]);
+?>
